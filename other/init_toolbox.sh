d02019e31991502bc3566e0b28f33f5dbf70a3ee
- <->
2025-11-08 23:54:22 +0100

    added flatpak (required by xdg-open inside toolbox)

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # cli tools used in bash init config
-sudo dnf install -y zoxide bat lsd tmux neovim
+sudo dnf install -y zoxide bat lsd tmux neovim flatpak
 
 # patch gui issues
 sudo dnf install -y ksshaskpass kde-runtime plasma-workspace
 
 # nice to have
 sudo dnf install -y htop
-
