ab1b8f310dc12ee5b86f02deba44a19e77a407a5
- <->
2025-11-09 06:34:24 +0100

    install codecs

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -8,3 +8,10 @@ sudo dnf install -y ksshaskpass kde-runtime plasma-workspace
 
 # nice to have
 sudo dnf install -y htop
+
+# enable rpm-fusion and install multimedia codecs
+sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+sudo dnf -y install ffmpeg --allowerasing
+sudo dnf -y install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
+
