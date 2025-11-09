5e829a97078c02c0d8cc424b4f7c90a5809a105a
- <->
2025-11-09 06:19:22 +0100

    remove flatpak

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cli tools used in bash init config
-sudo dnf install -y zoxide bat lsd tmux neovim flatpak
+sudo dnf install -y zoxide bat lsd tmux neovim
 
 # patch gui issues
 sudo dnf install -y ksshaskpass kde-runtime plasma-workspace
