be48bcf02f5c2737593d1d9a799786238ff88c25
- <->
2025-11-10 02:03:06 +0100

    download cloudflared too

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # cli tools used in bash init config
-sudo dnf install -y zoxide bat lsd tmux neovim
+sudo dnf -y install zoxide bat lsd tmux neovim
 
 # patch gui issues
-sudo dnf install -y ksshaskpass kde-runtime plasma-workspace
+sudo dnf -y install ksshaskpass kde-runtime plasma-workspace
 
 # nice to have
-sudo dnf install -y htop
+sudo dnf -y install htop
 
 # enable rpm-fusion and install multimedia codecs
 sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
@@ -15,3 +15,7 @@ sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-no
 sudo dnf -y install ffmpeg --allowerasing
 sudo dnf -y install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
 
+# download cloudflared
+curl -fsSl https://pkg.cloudflare.com/cloudflared.repo | sudo tee /etc/yum.repos.d/cloudflared.repo
+sudo dnf -y update
+sudo dnf -y install cloudflared
