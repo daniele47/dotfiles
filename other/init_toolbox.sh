378bd4c0827360b29f2df3d4ac1ce6f104215256
- <->
2025-11-10 02:07:19 +0100

    removed codecs

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -10,10 +10,10 @@ sudo dnf -y install ksshaskpass kde-runtime plasma-workspace
 sudo dnf -y install htop
 
 # enable rpm-fusion and install multimedia codecs
-sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf -y install ffmpeg --allowerasing
-sudo dnf -y install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
+# sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+# sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+# sudo dnf -y install ffmpeg --allowerasing
+# sudo dnf -y install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
 
 # download cloudflared
 curl -fsSl https://pkg.cloudflare.com/cloudflared.repo | sudo tee /etc/yum.repos.d/cloudflared.repo
