7542766c997180b2627a5ca42e5863e957853699
- <->
2025-11-11 15:51:16 +0100

    added c-dev tools to init script for toolbox container

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -9,11 +9,8 @@ sudo dnf -y install ksshaskpass kde-runtime plasma-workspace
 # nice to have
 sudo dnf -y install htop
 
-# enable rpm-fusion and install multimedia codecs
-# sudo dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-# sudo dnf -y install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-# sudo dnf -y install ffmpeg --allowerasing
-# sudo dnf -y install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
+# development tools
+sudo dnf install @c-development
 
 # download cloudflared
 curl -fsSl https://pkg.cloudflare.com/cloudflared.repo | sudo tee /etc/yum.repos.d/cloudflared.repo
