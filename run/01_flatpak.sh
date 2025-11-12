50e012ad1228912582a82f82506cb98151586660
- <->
2025-11-12 14:42:13 +0100

    autodownload easyeffect (it use qt now, thus it perfectly integrates into kde DE)

--- run/01_flatpak.sh
+++ run/01_flatpak.sh
@@ -13,5 +13,5 @@ sudo flatpak remote-delete fedora-testing || true
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # install necessary flatpaks
-flatpak install -y org.kde.okular org.kde.haruna org.kde.gwenview com.protonvpn.www org.libreoffice.LibreOffice com.github.vladimiry.ElectronMail
+flatpak install -y org.kde.okular org.kde.haruna org.kde.gwenview com.protonvpn.www org.libreoffice.LibreOffice com.github.vladimiry.ElectronMail com.github.wwmm.easyeffects
 
