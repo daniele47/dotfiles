29e472192fe9c8d5866bc10245140b351373bf3f
- <->
2025-11-13 17:17:06 +0100

    removed easyeffects, because it was bugged and busted music on the main speakers

--- run/01_flatpak.sh
+++ run/01_flatpak.sh
@@ -13,5 +13,5 @@ sudo flatpak remote-delete fedora-testing || true
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # install necessary flatpaks
-flatpak install -y org.kde.okular org.kde.haruna org.kde.gwenview com.protonvpn.www org.libreoffice.LibreOffice com.github.vladimiry.ElectronMail com.github.wwmm.easyeffects org.mozilla.firefox
+flatpak install -y org.kde.okular org.kde.haruna org.kde.gwenview com.protonvpn.www org.libreoffice.LibreOffice com.github.vladimiry.ElectronMail org.mozilla.firefox
 
