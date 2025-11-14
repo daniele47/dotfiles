936df4edb36cbab121099f7e8cd2134fd8fa86b0
- <->
2025-11-14 09:50:39 +0100

    fixed countdown script

--- other/countdown.sh
+++ other/countdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WAIT_UNTIL="9:44"
+[[ -z "$WAIT_UNTIL" ]] && WAIT_UNTIL="9:44"
 secs=1
 while [[ "$secs" -gt 0 ]]; do 
     secs="$(expr $(date -d "$WAIT_UNTIL" +%s) - $(date +%s))"
@@ -9,4 +9,4 @@ while [[ "$secs" -gt 0 ]]; do
 done
 echo -e "\r\e[KTime $WAIT_UNTIL reached!"
 
-return 0
+exit 0
