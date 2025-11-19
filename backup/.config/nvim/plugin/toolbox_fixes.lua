e7988a362b2655c73dd5c4f9857c9150fbceef30
- <->
2025-11-19 15:36:53 +0100

    space fix

--- backup/.config/nvim/plugin/toolbox_fixes.lua
+++ backup/.config/nvim/plugin/toolbox_fixes.lua
@@ -2,5 +2,5 @@ if vim.fn.filereadable('/run/.toolboxenv') == 0 or vim.fn.filereadable('/run/.co
     return
 end
 
-vim.g.netrw_browsex_viewer = "systemd-run --user --wait --quiet xdg-open"
+vim.g.netrw_browsex_viewer = "systemd-run --user --wait --quiet xdg-open "
 vim.ui.open = function(url) os.execute(vim.g.netrw_browsex_viewer .. url) end
