8799f7e344de3826ddba695900757383927a83d0
- <->
2025-11-19 16:02:37 +0100

    fixed vim.ui.open to fully normalize path

--- backup/.config/nvim/plugin/toolbox_fixes.lua
+++ backup/.config/nvim/plugin/toolbox_fixes.lua
@@ -3,4 +3,11 @@ if vim.fn.filereadable('/run/.toolboxenv') == 0 or vim.fn.filereadable('/run/.co
 end
 
 vim.g.netrw_browsex_viewer = "systemd-run --user --wait --quiet xdg-open "
-vim.ui.open = function(url) os.execute(vim.g.netrw_browsex_viewer .. url) end
+vim.ui.open = function(url) 
+    if vim.uv.fs_stat(url) then
+        local path = vim.fs.normalize(vim.fn.resolve(vim.fs.abspath(url)))
+        os.execute(vim.g.netrw_browsex_viewer .. vim.fn.shellescape(path))
+    else
+        os.execute(vim.g.netrw_browsex_viewer .. vim.fn.shellescape(url)) 
+    end
+end
