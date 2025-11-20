9d93dc3c29a71c774aab8230658ab2dc82c6b0d8
- <->
2025-11-20 23:26:23 +0100

    removed nicities, but making image heavy

--- other/init_toolbox.sh
+++ other/init_toolbox.sh
@@ -3,12 +3,6 @@
 # cli tools used in bash init config
 sudo dnf -y install zoxide bat lsd tmux neovim
 
-# nice to have
-sudo dnf -y install htop trash entr jq vidir yt-dlp
-
-# development tools
-sudo dnf -y install @c-development dig
-
 # download cloudflared
 curl -fsSl https://pkg.cloudflare.com/cloudflared.repo | sudo tee /etc/yum.repos.d/cloudflared.repo
 sudo dnf -y update
