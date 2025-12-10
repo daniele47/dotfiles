aa792dbd0c90088a889d5730642cc68c368126a1
- <->
2025-12-10 03:15:05 +0100

    replaced toolbox with distrobox

--- run/01_packages.sh
+++ run/01_packages.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # remove bloat
-sudo rpm-ostree override remove kde-connect-libs kde-connect kdeconnectd firefox firefox-langpacks kdebugsettings kjournald firewall-config plasma-drkonqi khelpcenter plasma-welcome plasma-welcome-fedora krfb krdp krfb-libs kcharselect
+sudo rpm-ostree override remove kde-connect-libs kde-connect kdeconnectd firefox firefox-langpacks kdebugsettings kjournald firewall-config plasma-drkonqi khelpcenter plasma-welcome plasma-welcome-fedora krfb krdp krfb-libs kcharselect toolbox
 
 # install system tools
-sudo rpm-ostree install neovim bat tmux htop trash-cli -y
+sudo rpm-ostree install neovim bat tmux htop trash-cli distrobox -y
