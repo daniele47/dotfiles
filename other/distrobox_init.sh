4b6f3c254cca4ae6987a7069f4a79bf2d8e27323
- <->
2025-12-14 05:58:39 +0100

    removed dolpgin and firefox from distrobox container

--- other/distrobox_init.sh
+++ other/distrobox_init.sh
@@ -4,13 +4,13 @@
 # distrobox create --unshare-all -H ~/.local/share/distrobox/fedora --no-entry --init --additional-packages systemd
 
 # basic packages
-sudo dnf --assumeyes install bat neovim tmux yt-dlp htop plasma-integration unison konsole dolphin firefox
-
-# codecs
-sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf --assumeyes install ffmpeg --allowerasing
-sudo dnf --assumeyes install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
+sudo dnf --assumeyes install bat neovim tmux yt-dlp htop plasma-integration unison konsole
 
 # netbird
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
+
+# codecs
+# sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+# sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+# sudo dnf --assumeyes install ffmpeg --allowerasing
+# sudo dnf --assumeyes install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
