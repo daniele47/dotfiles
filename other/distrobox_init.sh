cfd45a52169247bd86ce93703b3827d197500e88
- <->
2025-12-14 23:29:29 +0100

    do not install konsole in the distrobox

--- other/distrobox_init.sh
+++ other/distrobox_init.sh
@@ -4,13 +4,7 @@
 # distrobox create --unshare-all -H ~/.local/share/distrobox/fedora --no-entry --init --additional-packages systemd
 
 # basic packages
-sudo dnf --assumeyes install bat neovim tmux yt-dlp htop plasma-integration unison konsole
+sudo dnf --assumeyes install bat neovim tmux yt-dlp htop plasma-integration unison
 
 # netbird
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
-
-# codecs
-# sudo dnf --assumeyes install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-# sudo dnf --assumeyes install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-# sudo dnf --assumeyes install ffmpeg --allowerasing
-# sudo dnf --assumeyes install libheif-freeworld mesa-va-drivers-freeworld libavcodec-freeworld intel-media-driver
