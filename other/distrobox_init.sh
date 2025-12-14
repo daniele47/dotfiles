26738d2daeea6a51a268da3defdc15051197042e
- <->
2025-12-14 23:16:44 +0000

    updated distrobox init script comments

--- other/distrobox_init.sh
+++ other/distrobox_init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# to create the distrobox container (inspiration. modify command as needed)
-# distrobox create --unshare-all -H ~/.local/share/distrobox/fedora --no-entry --init --additional-packages systemd
+# to create the distrobox container (some flags might need changing):
+#   - distrobox create --unshare-all -H ~/.local/share/distrobox/fedora --no-entry --init --additional-packages systemd
 
 # basic packages
 sudo dnf --assumeyes install bat neovim tmux yt-dlp htop unison
 
 # netbird
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
+
+# to login into netbird network:
+#   - sudo netbird up
