c7ee9f35c5e346cd22f608a9cc61fe99438feccf
- <->
2025-12-14 23:36:38 +0100

    removed plasmap=-integration in distrobox init: too much bloat

--- other/distrobox_init.sh
+++ other/distrobox_init.sh
@@ -4,7 +4,7 @@
 # distrobox create --unshare-all -H ~/.local/share/distrobox/fedora --no-entry --init --additional-packages systemd
 
 # basic packages
-sudo dnf --assumeyes install bat neovim tmux yt-dlp htop plasma-integration unison
+sudo dnf --assumeyes install bat neovim tmux yt-dlp htop unison
 
 # netbird
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
