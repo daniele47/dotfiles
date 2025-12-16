2e7e2f282c02fb4944b8331dad97d032a58fbd7b
- <->
2025-12-16 10:05:02 +0100

    removed hacky last_upgrade date check

--- backup/.bashrc.d/04_updates.sh
+++ backup/.bashrc.d/04_updates.sh
@@ -4,18 +4,5 @@ function upgrade(){
     sudo sh -c '
         rpm-ostree update
         flatpak update -y
-    ' && distrobox upgrade --all &&
-    date +%s > "${XDG_STATE_HOME:-$HOME/.local/state}/last_upgrade_run"
+    ' && distrobox upgrade --all
 }
-
-days=3
-last=$(cat "${XDG_STATE_HOME:-$HOME/.local/state}/last_upgrade_run" 2>/dev/null)
-now=$(date +%s)
-
-if ! [[ "$now" -gt "$last" ]] 2>/dev/null; then
-    echo -e "\e[1;33mWarning: invalid last upgrade run date! run 'upgrade' to update...\e[m" 
-elif (( now - last > $days*24*60*60 )); then 
-    echo -e "\e[1;33mWarning: last manual update run more then $days days ago. run 'upgrade' to update...\e[m" 
-fi
-
-unset days last now
