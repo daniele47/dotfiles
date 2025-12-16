29a83355534ccbc97511049962a6c278a49f4ad0
- <->
2025-12-16 10:01:10 +0100

    fix upgrade function

--- backup/.bashrc.d/04_updates.sh
+++ backup/.bashrc.d/04_updates.sh
@@ -4,17 +4,17 @@ function upgrade(){
     sudo sh -c '
         rpm-ostree update
         flatpak update -y
-    '
-    distrobox upgrade --all
+    ' && distrobox upgrade --all &&
     date +%s > "${XDG_STATE_HOME:-$HOME/.local/state}/last_upgrade_run"
 }
 
 days=3
-last=$(date -r "${XDG_STATE_HOME:-$HOME/.local/state}/last_upgrade_run" +%s 2>/dev/null)
-last="${last:-0}"
+last=$(cat "${XDG_STATE_HOME:-$HOME/.local/state}/last_upgrade_run" 2>/dev/null)
 now=$(date +%s)
 
-if (( now - last > $days*24*60*60 )); then 
+if ! [[ "$now" -gt "$last" ]] 2>/dev/null; then
+    echo -e "\e[1;33mWarning: invalid last upgrade run date! run 'upgrade' to update...\e[m" 
+elif (( now - last > $days*24*60*60 )); then 
     echo -e "\e[1;33mWarning: last manual update run more then $days days ago. run 'upgrade' to update...\e[m" 
 fi
 
