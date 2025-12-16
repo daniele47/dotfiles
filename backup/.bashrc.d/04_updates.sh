2947602a120b8dbd46a35443d6beb122e629477f
- <->
2025-12-16 11:59:28 +0100

    now bash checks and warns if the latest upgrade happened more then N days ago

--- backup/.bashrc.d/04_updates.sh
+++ backup/.bashrc.d/04_updates.sh
@@ -1,8 +1,61 @@
 #!/bin/bash
 
 function upgrade(){
-    sudo sh -c '
-        rpm-ostree update
-        flatpak update -y
-    ' && distrobox upgrade --all
+    sudo sh -c 'rpm-ostree update && flatpak update -y' && 
+        distrobox upgrade --all && 
+        upgrade-check updated
 }
+
+function upgrade-check(){
+    local -r last_update_path="${XDG_STATE_HOME:-$HOME/.local/state}/last_update_time"
+    local -r current_time="$(date +%s)"
+
+    # create last upgrade file
+    mkdir -p "$(dirname "$last_update_path")"
+    touch "$last_update_path"
+    
+    # clean file, if the content is invalid
+    local last_update=$(<"$last_update_path")
+    if [[ ! -s "$last_update_path" ]]; then
+        if [[ ! "$last_update" =~ ^[0-9]+$ ]] || (( last_update <= 0 )) || (( last_update > current_time )); then
+            echo > "$last_update_path"
+        fi
+    fi
+    last_update=$(<"$last_update_path")
+
+    # actual execution
+    [[ "$#" -ge 2 ]] && echo -e '\e[1;31monly a single parameter is allowed\e[m' && return 1
+    case "$1" in 
+        "") 
+            if [[ -z "$last_update" ]]; then
+                echo "EMPTY"
+            else
+                local -r diff="$("$FUNCNAME" diff)"
+                local -r secs="$(( diff % 60 ))"
+                local -r minutes="$(( (diff / 60) % 60 ))"
+                local -r hours="$(( (diff / 3600) % 24 ))"
+                local -r days="$(( diff / 86400 ))"
+                [[ $days -gt 0 ]] && printf '%dd ' $days
+                [[ $hours -gt 0 ]] && printf '%dh ' $hours
+                [[ $minutes -gt 0 ]] && printf '%dm ' $minutes
+                printf '%ds have passed since the latest time the upgrade\n' $secs
+            fi
+            ;;
+        updated) date +%s > "$last_update_path" ;;
+        diff) echo "$(( current_time - last_update ))" ;;
+        warn)
+            if [[ -z "$last_update" ]]; then
+                echo -e "\e[1;33mLatest upgrade unknown. Run upgrade...\e[m"
+            else
+                local -r diff="$("$FUNCNAME" diff)"
+                local -r days=3
+                if (( diff > days*24*60*60 )); then
+                    echo -e "\e[1;33mMore then $days have passed since latest upgrade. run upgrade...\e[m"
+                fi
+            fi
+            ;;
+        *) echo -e "\e[1;31minvalid parameter '$1'\e[m" && return 1 ;;
+    esac
+}
+
+upgrade-check warn
