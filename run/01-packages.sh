356bb4c8c763558eb33963ac43c3f9537799ed0b
- <->
2025-12-18 19:23:25 +0100

    download rclone too

--- run/01-packages.sh
+++ run/01-packages.sh
@@ -8,5 +8,5 @@ sudo apt upgrade -y
 
 # various utils
 sudo apt install -y htop lm-sensors neovim tree git tmux
-sudo apt install -y bat zoxide
+sudo apt install -y bat zoxide rclone
 sudo apt install -y rpi-connect-lite
