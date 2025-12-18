117c73940e21223afcf89faae355d21e443819fd
- <->
2025-12-18 01:49:04 +0100

    remove more packages

--- run/01_packages.sh
+++ run/01_packages.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # remove bloat
-sudo rpm-ostree override remove kde-connect-libs kde-connect kdeconnectd firefox firefox-langpacks kdebugsettings kjournald firewall-config plasma-drkonqi khelpcenter plasma-welcome plasma-welcome-fedora krfb krdp krfb-libs kcharselect toolbox plasma-discover plasma-discover-rpm-ostree plasma-discover-notifier plasma-discover-kns plasma-discover-flatpak
+sudo rpm-ostree override remove kde-connect-libs kde-connect kdeconnectd firefox firefox-langpacks kdebugsettings kjournald firewall-config plasma-drkonqi khelpcenter plasma-welcome plasma-welcome-fedora krfb krdp krfb-libs kcharselect toolbox plasma-discover plasma-discover-rpm-ostree plasma-discover-notifier plasma-discover-kns plasma-discover-flatpak plasma-browser-integration fedora-chromium-config-kde
 
 # install system tools
 sudo rpm-ostree install neovim bat -y
