c4eeba7eaecc3875fb10d04ceebbcf58ca846fb7
- <->
2025-12-20 01:42:03 +0100

    added sync script

---
+++ other/sync.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# variables
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+REMOTE_NAME="$1"
+SYNC_DIR="$2"
+
+# checks
+[[ "$#" -gt 2 ]] && echo 'this scripts allows only 2 parameters' && exit 1 || true
+
+# get missing parameters from user
+until [[ -n "$REMOTE_NAME" ]]; do read -rp "What is the rclone remote name? " REMOTE_NAME; done
+until [[ -n "$SYNC_DIR" ]]; do read -rp "What is sync direction? " SYNC_DIR; done
+
+# sync with rclone remote
+case "$SYNC_DIR" in
+    get|pull) 
+        read -rp "WARNING: Are you sure you want to pull remote '$REMOTE_NAME:/sync' into local '$SCRIPT_DIR'? " answer
+        [[ "${answer,,}" != "y" ]] && exit 0
+        rclone sync "$REMOTE_NAME:/sync" "$SCRIPT_DIR" --progress 
+        ;;
+    put|push) 
+        read -rp "WARNING: Are you sure you want to push local '$SCRIPT_DIR' into remote '$REMOTE_NAME:/sync'? " answer
+        [[ "${answer,,}" != "y" ]] && exit 0
+        rclone sync "$SCRIPT_DIR" "$REMOTE_NAME:/sync" --progress 
+        ;;
+    *) echo 'invalid sync direction'; exit 1;;
+esac
