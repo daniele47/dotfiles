6830d61afa8b86314607648501961b265f0e77c4
- <->
2025-12-29 22:29:32 +0100

    now box is safer, with user and root shell split

--- backup/.config/bashrc.d/04_gui_containers.sh
+++ backup/.config/bashrc.d/04_gui_containers.sh
@@ -9,17 +9,18 @@ function box() {
     local -r label="function=box-39ff3913-0d21-4443-b10a-094fc3fc4d81" 
     case "$1" in 
         _create)
-            podman run -d \
+            local -r CONTAINER="$(podman run -d \
                 -v "$XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR:ro" \
                 -v "/tmp/.X11-unix:/tmp/.X11-unix:ro" \
                 --security-opt "label=type:container_runtime_t" \
                 --name "gui-box-$(date +%Y-%m-%d-%H-%M-%S)" \
                 --device /dev/dri \
-                --workdir /root \
+                --userns=keep-id \
                 --label "$label" \
                 --init \
                 fedora \
-                sleep infinity
+                sleep infinity)"
+            podman exec -u 0 -it "$CONTAINER" sh -c "cp -r /etc/skel/ /home/$(whoami) ; chown -R $(id -u):$(id -g) /home/$(whoami); chmod 700 /home/$(whoami)"
             ;;
         _list) podman ps -aq --filter "label=$label" ;;
         _list_fmt) podman ps -a --filter "label=$label" ;;
@@ -27,30 +28,46 @@ function box() {
         ls) "$FUNCNAME" _list_fmt ;;
         rm) 
             if [[ "$("$FUNCNAME" _count)" -gt 0 ]]; then
-                read -rp "Are you sure you want to delete the container? [Y/n] " answer
-                if [[ "${answer,,}" == y ]]; then
-                    for container in $("$FUNCNAME" _list); do podman rm -f "$container"; done >/dev/null
+                if [[ "$2" != "-f" ]]; then
+                    read -rp "Are you sure you want to delete the container? [Y/n] " answer
+                    if [[ "${answer,,}" != y ]]; then
+                        return 0
+                    fi
                 fi
+                echo "Removing container..."
+                for container in $("$FUNCNAME" _list); do podman rm -f "$container"; done >/dev/null
             fi
             ;;
-        run|"") 
+        ""|run|root) 
             case "$("$FUNCNAME" _count)" in
                 0) "$FUNCNAME" _create; ;;
                 1) ;;
                 *) echo -e "\e[1;31mmultiple container running\e[m" 1>&2; return 1 ;;
             esac
             podman start "$("$FUNCNAME" _list)" >/dev/null
-            podman exec -it \
-                -e "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" \
-                -e "DISPLAY=$DISPLAY" \
-                -e "XAUTHORITY=$XAUTHORITY" \
-                -e "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
-                -e "PIPEWIRE_SOCKET=$XDG_RUNTIME_DIR/pipewire-0" \
-                "$("$FUNCNAME" _list)" bash
+            case "$1" in
+                ""|run)
+                    podman exec -it \
+                        -e "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" \
+                        -e "DISPLAY=$DISPLAY" \
+                        -e "XAUTHORITY=$XAUTHORITY" \
+                        -e "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
+                        -e "PIPEWIRE_SOCKET=$XDG_RUNTIME_DIR/pipewire-0" \
+                        -e "HOME=/home/$(whoami)" \
+                        -w "/home/$(whoami)" \
+                        "$("$FUNCNAME" _list)" bash
+                    ;;
+                root) podman exec -u 0 -it -w /root "$("$FUNCNAME" _list)" bash ;;
+            esac
             ;;
-        stop) for container in $("$FUNCNAME" _list); do podman stop "$container"; done >/dev/null ;;
+        stop) 
+            if [[ "$("$FUNCNAME" _count)" -gt 0 ]]; then
+                echo "Stopping container..."
+                for container in $("$FUNCNAME" _list); do podman stop "$container"; done >/dev/null ;
+            fi
+            ;;
         *) echo -e "\e[1;31minvalid args\e[m" 1>&2; return 1 ;;
-        esac
-    }
+    esac
+}
 
 complete -W "ls rm run stop" box
