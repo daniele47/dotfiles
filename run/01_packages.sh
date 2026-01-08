b269696adaa9558bd9f7199fd3ca9df12c8a5735
- <->
2026-01-08 17:52:40 +0100

    non versioned rpm-fusion repo packages

--- run/01_packages.sh
+++ run/01_packages.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-# add rpm-fusion repos
+# add rpm-fusion repos and set the noversioned repo package
 sudo rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
                         "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 sudo rpm-ostree apply-live --allow-replacement || true
+sudo rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release
 
 # install ffmpeg for full multimedia codecs support
 sudo rpm-ostree override remove \
