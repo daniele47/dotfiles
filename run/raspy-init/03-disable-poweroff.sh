86e0c360f25008671d2302b2e7c88e3bc267684a
Private <private@invalid.local>
2026-05-05 21:34:01 +0000

    updated raspby init script

--- run/raspy-init/03-disable-poweroff.sh
+++ run/raspy-init/03-disable-poweroff.sh
@@ -5,4 +5,4 @@ set -e
 # disable poweroff and halt (avoid making raspberry pi unreachable)
 sudo systemctl mask poweroff.target
 sudo systemctl mask halt.target
-
+echo "poweroff and halt disabled!"
