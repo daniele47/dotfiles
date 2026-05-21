fa25bfe46fbe913248c1c61f83f564b4a5633a85
Private <private@invalid.local>
2026-05-22 00:18:33 +0200

    added osc52 to raspberry pi neovim config

--- backup/raspberry/.config/nvim/init.lua
+++ backup/raspberry/.config/nvim/init.lua
@@ -34,3 +34,17 @@ vim.opt.mouse = "a"
 vim.opt.swapfile = false
 vim.opt.fillchars = { eob = " " }
 vim.opt.signcolumn = "no"
+
+local function copy(lines, _)
+  require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {['+'] = copy, ['*'] = copy},
+  paste = {['+'] = paste, ['*'] = paste},
+}
