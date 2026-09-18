42b688d5c4b88bfecdc007c4dab241fda33036ff
Private <private@invalid.local>
2026-09-18 21:56:27 +0200

    updated install script to download uv + removed direnv

--- run/tmp-kdelinux/.init/01_pkg.sh
+++ run/tmp-kdelinux/.init/01_pkg.sh
@@ -7,10 +7,10 @@ BASE_PKGS=(base base-devel glibc glibc-locales)
 CLI_TOOLS=(
     zsh bash-completion lsd bat trash-cli git
     tmux neovim tree man tldr wl-clipboard jq
-    wget zip unzip podman htop openssh entr direnv
+    wget zip unzip podman htop openssh entr
 )
 GUI_TOOLS=(plasma-integration ksshaskpass)
-PROG_LANG=(python rustup go nodejs npm)
+PROG_LANG=(python uv rustup go nodejs npm)
 
 # install packages
 sudo pacman -Syu --needed --noconfirm "${BASE_PKGS[@]}" "${CLI_TOOLS[@]}" "${GUI_TOOLS[@]}" "${PROG_LANG[@]}"
